75f2d0303d02529f2a9d85d92ed0af277bec832b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-04 18:24:49 -0400

    added example createjs code

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,5 +1,26 @@
+/// <reference path="_reference.ts"/>
+// make a reference to the canvas element
+var canvas = document.getElementById("canvas");
+// create a reference to a stage container
+var stage;
+var helloLabel;
 function init() {
-    console.log("Game Started Now!");
+    stage = new createjs.Stage(canvas); // instatiate the stage container
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", gameLoop); // create an event listener for the tick event
+    main(); // call the main game function
+}
+function gameLoop() {
+    helloLabel.rotation += 5;
+    stage.update(); // refreshes the stage
+}
+function main() {
+    helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    helloLabel.x = 320;
+    helloLabel.y = 240;
+    stage.addChild(helloLabel);
 }
 window.addEventListener("load", init);
 //# sourceMappingURL=game.js.map
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,7 +1,34 @@
+/// <reference path="_reference.ts"/>
 
+// make a reference to the canvas element
+let canvas:HTMLElement = document.getElementById("canvas");
+// create a reference to a stage container
+let stage:createjs.Stage;
+
+let helloLabel:createjs.Text;
 
 function init():void {
-    console.log("Game Started Now!");
+    stage = new createjs.Stage(canvas); // instatiate the stage container
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", gameLoop); // create an event listener for the tick event
+    main(); // call the main game function
+}
+
+function gameLoop():void {
+
+    helloLabel.rotation += 5;
+
+
+    stage.update(); // refreshes the stage
+}
+
+function main():void {
+    helloLabel =  new createjs.Text("Hello World!","40px Consolas", "#000000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    helloLabel.x = 320;
+    helloLabel.y = 240;
+    stage.addChild(helloLabel);
 }
 
 window.addEventListener("load", init);
